feb61ea0f02563eaec0019bb865db7a933785147
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-05-08 19:43:13 -0500

    Update product_list.dart

--- lib/src/widgets/product_list.dart
+++ lib/src/widgets/product_list.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+
+  ProductList({required this.products});
+
+  // Your product list widget logic goes here
+}
 
