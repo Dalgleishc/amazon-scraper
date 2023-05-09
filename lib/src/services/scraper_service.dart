d6fb987d201767bc6157b3a603a8f46c2ed7600b
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-05-08 19:42:13 -0500

    Update scraper_service.dart

--- lib/src/services/scraper_service.dart
+++ lib/src/services/scraper_service.dart
@@ -1,1 +1,15 @@
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart';
+import 'package:beautifulsoup/beautifulsoup.dart';
+import '../models/product.dart';
+
+class ScraperService {
+  Future<List<Product>> scrapeAmazon(String url, double minPrice, double maxPrice, double minRating) async {
+    List<Product> products = [];
+
+    // Your web scraping logic goes here
+
+    return products;
+  }
+}
 
