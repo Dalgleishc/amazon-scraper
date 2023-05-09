c961706252a052d4366fa8eee66bab0546363b89
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-05-08 19:43:34 -0500

    Update main.dart

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,1 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'src/services/scraper_service.dart';
+import 'src/widgets/input_form.dart';
+import 'src/widgets/product_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon Scraper',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider(
+        create: (context) => ScraperService(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Amazon Scraper'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                InputForm(),
+                ProductList(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
 
