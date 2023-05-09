b0acc0bfb01c331321f469871775b04594cbbd98
Caleb Dalgleish <Dalgleishc@uchicago.edu>
2023-05-08 19:41:49 -0500

    Update product.dart

--- lib/src/models/product.dart
+++ lib/src/models/product.dart
@@ -1,1 +1,9 @@
+class Product {
+  final String title;
+  final double price;
+  final double rating;
+  final String imageUrl;
+
+  Product({required this.title, required this.price, required this.rating, required this.imageUrl});
+}
 
